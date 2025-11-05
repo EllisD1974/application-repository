6044d7b504320a51b6600b67099efc3f9680475d
Nathan Loewenthal <nathanloewe@gmail.com>
2025-11-04 20:56:00 -0500

    Using change logs in example script

--- module/tests/example_usage.py
+++ module/tests/example_usage.py
@@ -1,24 +1,27 @@
-from apprepocl import AppRepoClient
+from apprepocl import AppRepoClient, Log, Changelog
 
 client = AppRepoClient("http://localhost:8000")
 
+
+pause = 1
+
 # List applications
 apps = client.list_applications()
-print("Available applications:", apps)
+print(f"Available applications: {apps}")
 
 # Upload a new app
 resp = client.upload_application(
     application_name="MyApp",
     version="1.0.0",
-    file_path=r"C:\developers\repos\application-repository\module\tests\test_myapp.test",
+    file_path=r"C:\developer\repos\application-repository\module\tests\test_myapp.test",
     clobber=False,
-    auto_increment_index=None,
+    auto_increment_index=-1,
 )
-print("Upload response:", resp)
-
-# # Download an app to downloads directory
-# file_path = client.download_application("MyApp", "1.0.0", "downloads/")
-# print(f"Downloaded to {file_path}")
+print(f"Upload response: {resp}")
+# from pprint import pprint
+# Download an app to downloads directory
+file_path = client.download_application("MyApp", "1.0.0", "downloads/")
+print(f"Downloaded to {file_path}")
 
 # # Download an app to downloads directory with new name
 # file_path = client.download_application("MyApp", "1.0.0", "downloads/new_name.txt")
@@ -26,4 +29,11 @@ print("Upload response:", resp)
 
 # List applications
 apps = client.list_applications()
-print("Available applications:", apps)
+print(f"Available applications: {apps}")
+
+# Add some change logs
+client.add_log("MyApp", Log("1.0.0", "Changed thing number 1", ticket="MT-1234"))
+client.add_log("MyApp", Log("1.0.0", "Changed thing number 2", ticket="MT-5678"))
+
+logs = client.get_logs("MyApp", "1.0.0")
+print(f"Change logs for that app/version: {logs}")
